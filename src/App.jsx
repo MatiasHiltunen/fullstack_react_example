f09cf7b463bf68ad3c7a664c7c12c91fcf03ccd5
matiashiltunen <26164588+MatiasHiltunen@users.noreply.github.com>
2025-09-24 16:01:29 +0300

    lecture_end

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+function FooterNav({ allLinks }) {
 
-  const [count, setCount] = useState(0)
+  const [title, ...links] = allLinks
 
-  const onButtonClick = () => {
-    setCount((curretCount) => curretCount + 1)
-  }
+  const linkComponents = links.map((link) => <a className="link link-hover">{link}</a>)
 
-  return (
-    <div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={onButtonClick}>
-          count is {count} and double count {count * 2}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <nav>
+    <h6 className="footer-title">{title}</h6>
+    {linkComponents}
+  </nav>
 }
 
-export default App
+function Footer() {
+
+  const allLinks = [
+    "Palvelut",
+    "Branding",
+    "Design",
+    "Marketing",
+    "Advertisement",
+  ]
+
+  return <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+    <FooterNav allLinks={allLinks}></FooterNav>
+    <FooterNav allLinks={allLinks}></FooterNav>
+    <FooterNav allLinks={allLinks}></FooterNav>
+  </footer>
+}
+
+export default function App() {
+
+
+  return <>
+
+    <header>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path> </svg>
+          </button>
+        </div>
+        <div className="flex-1">
+          <a className="btn btn-ghost text-xl">daisyUI</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
+          </button>
+        </div>
+      </div>
+    </header>
+    <main className="h-screen">
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+            {/* row 2 */}
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+            {/* row 3 */}
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
+
+    <Footer></Footer>
+
+  </>
+
+}
